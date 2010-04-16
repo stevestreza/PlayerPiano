f1f2485c952046856bfbd29e19df240d9ce5991f
Steve Streza <stevestreza@gmail.com>
2010-04-16 19:33:33 -0400

    Updated licensing information

--- NSWindow+TitlebarAccessory.h
+++ NSWindow+TitlebarAccessory.h
@@ -3,7 +3,10 @@
 //  PlayerPiano
 //
 //  Created by Steve Streza on 2/14/10.
-//  Copyright 2010 Villainware. All rights reserved.
+//  Copyright 2010 Villainware.
+//
+//  See LICENSE.md file in the PlayerPiano source directory, or at
+//  http://github.com/amazingsyco/PlayerPiano/LICENSE.md
 //
 
 #import <Cocoa/Cocoa.h>
--- PPFileHandleLineBuffer.m
+++ PPFileHandleLineBuffer.m
@@ -3,7 +3,10 @@
 //  PlayerPiano
 //
 //  Created by Steve Streza on 2/13/10.
-//  Copyright 2010 Villainware. All rights reserved.
+//  Copyright 2010 Villainware.
+//
+//  See LICENSE.md file in the PlayerPiano source directory, or at
+//  http://github.com/amazingsyco/PlayerPiano/LICENSE.md
 //
 
 #import "PPFileHandleLineBuffer.h"
